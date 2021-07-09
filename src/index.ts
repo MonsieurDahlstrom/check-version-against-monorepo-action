4ba7d881191a689f0916f623f9bf57753c043908
Mathias Dahlström <mathias@monsieurdahlstrom.com>
2021-07-09 16:42:53 +0200

    adding loading of package.json

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,24 @@
 import * as core from "@actions/core";
-import * as github from "@actions/github";
+import fs from "fs";
+import path from "path";
 
 try {
   // `who-to-greet` input defined in action metadata file
+  //default state is false
+  core.setOutput("newer", false);
   const repoReference = core.getInput("repository");
   const folderReference = core.getInput("directory");
   const commitReference = core.getInput("ref");
   console.log(`${repoReference} ${folderReference} ${commitReference}!`);
-  core.setOutput("newer", false);
+  const fileBuffer: Buffer = fs.readFileSync(
+    path.join(folderReference, "package.json")
+  );
+  const packageDescription = JSON.parse(fileBuffer.toString("utf8"));
+  console.log(packageDescription);
+
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  //const payload = JSON.stringify(github.context.payload, undefined, 2);
+  //console.log(`The event payload: ${payload}`);
 } catch (error) {
   console.log(core.setFailed);
   console.log(error);
