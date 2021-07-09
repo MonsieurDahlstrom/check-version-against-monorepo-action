932e72fe9408b4845033cdd6e371e8f4d2686deb
Mathias Dahlström <mathias@monsieurdahlstrom.com>
2021-07-09 15:50:50 +0200

    updated logging

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ try {
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
-  console.error(error);
+  console.log(core);
+  console.log(error);
   core.setFailed(error.message);
 }
