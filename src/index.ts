eb0cde1b2c6719126bf7fe075798392828057c86
Mathias Dahlström <mathias@monsieurdahlstrom.com>
2021-07-10 10:54:28 +0200

    adding graphql query

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
 import * as core from "@actions/core";
+import { RetriveLatestPublishedVersion } from "./graphql";
 import { readPackageJson } from "./repository";
 
 try {
   // `who-to-greet` input defined in action metadata file
   //default state is false
   core.setOutput("newer", false);
-  const repoReference = core.getInput("repository");
+  const repoReference = core.getInput("repository").split("/");
   const commitReference = core.getInput("ref");
   console.log(`${repoReference} ${commitReference}!`);
   const nameAndVersion = readPackageJson();
-  console.log(nameAndVersion);
-
+  RetriveLatestPublishedVersion({
+    repoOwner: repoReference[0],
+    repoName: repoReference[1],
+    packageName: nameAndVersion.name,
+  });
   // Get the JSON webhook payload for the event that triggered the workflow
   //const payload = JSON.stringify(github.context.payload, undefined, 2);
   //console.log(`The event payload: ${payload}`);
